15f573293bb51394d1a4ed0b363ca0de77d9bb91
Bartollo3 <bdziechciarski@gmail.com>
2026-08-07 22:42:16 +0200

    update skle[p

--- scripts/sklepito.gd
+++ scripts/sklepito.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
-var coins = 100
+var coins: int = 100
+var tool1: bool = false
+var tool2: bool = false
+var tool3: bool = false
 
-# Called when the node enters the scene tree for the first time.
+# TODO: tu bedzie sie wczytywalo ilosc kasy z poprzedniego lvla
 func _ready() -> void:
 	pass # Replace with function body.
 
@@ -17,6 +20,7 @@ func _on_button_1_pressed() -> void:
 		coins -= 10
 		$Monety.text = "Monety: %s" %coins
 		$Button1.visible = false
+		tool1 = true
 	else:
 		pass
 
@@ -25,6 +29,7 @@ func _on_button_2_pressed() -> void:
 		coins -= 20
 		$Monety.text = "Monety: %s" %coins
 		$Button2.visible = false
+		tool2 = true
 	else:
 		pass
 
@@ -33,5 +38,6 @@ func _on_button_3_pressed() -> void:
 		coins -= 20
 		$Monety.text = "Monety: %s" %coins
 		$Button3.visible = false
+		tool3 = true
 	else:
 		pass
