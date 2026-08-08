0c9d487224b099c7e4628bcf34597d3265989e37
Terra <106891646+TerraInCaelo@users.noreply.github.com>
2026-08-08 02:31:57 +0200

    Update DualGrid.gd

--- Terra Nie ruszac/DualGrid.gd
+++ Terra Nie ruszac/DualGrid.gd
@@ -5,25 +5,25 @@ extends Node2D
 @export var tilesets: TileSet
 
 var neighbours_to_index: Dictionary = {
-	[false, false, false, false]: 0,
-	[true,  false, false, false]: 1,
-	[false, true,  false, false]: 2,
-	[true,  true,  false, false]: 3,
+	[false, false, true, false]: 0,
+	[true,  true, false, false]: 1,
+	[false, true,  true, true]: 2,
+	[false,  true,  true, false]: 3,
 
-	[false, false, true,  false]: 4,
-	[true,  false, true,  false]: 5,
-	[false, true,  true,  false]: 6,
-	[true,  true,  true,  false]: 7,
+	[false, true, false,  true]: 4,
+	[true,  true, true,  false]: 5,
+	[true, true,  true,  true]: 6,
+	[true,  false,  true,  true]: 7,
 
-	[false, false, false, true]: 8,
+	[true, false, false, false]: 8,
 	[true,  false, false, true]: 9,
-	[false, true,  false, true]: 10,
-	[true,  true,  false, true]: 11,
+	[true, true,  false, true]: 10,
+	[false,  false,  true, true]: 11,
 
-	[false, false, true,  true]: 12,
-	[true,  false, true,  true]: 13,
-	[false, true,  true,  true]: 14,
-	[true,  true,  true,  true]: 15,
+	[false, true, false,  false]: 12,
+	[true,  false, true,  false]: 13,
+	[false, false,  false,  true]: 14,
+	[false,  false,  false,  false]: 15,
 }
 
 func get_tile(layer: int, neighbours: Array[TileTypes.Type]) -> TileData:
