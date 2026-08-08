a2436a708954d3f628173866488ee83383961121
Albert Przytuła <przytulaalbert@gmail.com>
2026-08-08 05:00:22 +0200

    Revert "dual grid dziala"
    This reverts commit 09919d27a40acfef2aed57f58802136ad3f99fe9.

--- Terra Nie ruszac/DualGrid.gd
+++ Terra Nie ruszac/DualGrid.gd
@@ -5,24 +5,24 @@ extends Node2D
 @export var tileset: TileSet
 
 var neighbours_to_index: Dictionary = {
-	[false, false, false, true]: 0,#
+	[false, false, true, false]: 0,
 	[true,  true, false, false]: 1,
-	[true, false,  true, true]: 2,
-	[true,  false, false, true]: 3,
+	[false, true,  true, true]: 2,
+	[false,  true,  true, false]: 3,
 
-	[true,  false, true,  false]: 4,
-	[true,  true, false,  true]: 5,
+	[false, true, false,  true]: 4,
+	[true,  true, true,  false]: 5,
 	[true, true,  true,  true]: 6,
-	[false, true,  true, true]: 7,
+	[true,  false,  true,  true]: 7,
 
-	[false, true, false, false]: 8,
-	[false,  true, true, false]: 9,
-	[true,  true, true,  false]: 10,
+	[true, false, false, false]: 8,
+	[true,  false, false, true]: 9,
+	[true, true,  false, true]: 10,
 	[false,  false,  true, true]: 11,
 
-	[true, false, false, false]: 12,
-	[false,  true, false,  true]: 13,
-	[false, false, true, false]: 14,#
+	[false, true, false,  false]: 12,
+	[true,  false, true,  false]: 13,
+	[false, false,  false,  true]: 14,
 	[false,  false,  false,  false]: 15,
 }
 
